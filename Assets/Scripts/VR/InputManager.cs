b78c6d965d95941fe215d65a6f850421ba20f9c2
Martin Keesom <25179@ma-web.nl>
2019-03-11 09:54:49 +0100

    Keuken zit nu in de game

---
+++ Assets/Scripts/VR/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Control Ìnformation Trackpad!
+/// 
+/// Left Controller Trackpad (2)	Axis2D.PrimaryThumbstick	Left Controller Trackpad	Horizontal Movement	    1	–1.0 to 1.0
+/// Left Controller Trackpad(2)     Axis2D.PrimaryThumbstick Left Controller Trackpad       Vertical Movement       2	–1.0 to 1.0
+/// Right Controller Trackpad(2)    Axis2D.SecondaryThumbstick Right Controller Trackpad    Horizontal Movement     4	–1.0 to 1.0
+/// Right Controller Trackpad(2)    Axis2D.SecondaryThumbstick Right Controller Trackpad    Vertical Movement       5	–1.0 to 1.0
+/// </summary>
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
