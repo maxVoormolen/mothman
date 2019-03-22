a585921a2e21828414cd8490d87a0dedd0d008d8
Martin Keesom <25179@ma-web.nl>
2019-03-22 11:33:14 +0100

    Grab and drop implanted

--- Assets/Scripts/VR/Hand.cs
+++ Assets/Scripts/VR/Hand.cs
@@ -7,7 +7,8 @@ public class Hand : MonoBehaviour
 {
     public SteamVR_Action_Boolean m_GrabAction = null;
 
-    private GameObject model;
+    private SteamVR_RenderModel controllerModel;
+    
     private SteamVR_Behaviour_Pose m_Pose = null;
     private FixedJoint m_joint = null;
 
@@ -18,16 +19,15 @@ public class Hand : MonoBehaviour
     {
         m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
         m_joint = GetComponent<FixedJoint>();
+        controllerModel = GetComponentInChildren<SteamVR_RenderModel>();
+
     }
-    private void Start()
-    {
-        model = GameObject.FindWithTag("controlerModel");
-    }
+
 
     private void Update()
     {
-       // get trigger down input
-       if(m_GrabAction.GetStateDown(m_Pose.inputSource))
+        // get trigger down input
+        if (m_GrabAction.GetStateDown(m_Pose.inputSource))
         {
             print(m_Pose.inputSource + " Trigger Down");
             Pickup();
@@ -44,13 +44,13 @@ public class Hand : MonoBehaviour
         //tag check
         if (!other.gameObject.CompareTag("cube"))
             return;
-        
+
         m_contactInteracitbles.Add(other.gameObject.GetComponent<Interactible>());
     }
     private void OnTriggerExit(Collider other)
     {
         if (!other.gameObject.CompareTag("cube"))
-        return;
+            return;
 
         m_contactInteracitbles.Remove(other.gameObject.GetComponent<Interactible>());
     }
@@ -67,46 +67,39 @@ public class Hand : MonoBehaviour
         if (m_curentInteractible.m_activeHand)
             m_curentInteractible.m_activeHand.Drop();
 
-        //posision
-
-
-        //nodig? test later
-        //m_curentInteractible.transform.position = transform.position;
-        //of...
-        // m_curentInteractible.transform.position = transform.forward + new Vector3(0,0,1);
-
         // attach
         Rigidbody targetBody = m_curentInteractible.GetComponent<Rigidbody>();
         m_joint.connectedBody = targetBody;
 
         //set active hand
-        model.SetActive(false);
         m_curentInteractible.m_activeHand = this;
+        controllerModel.gameObject.SetActive(false);
     }
     public void Drop()
     {
         //null check
         if (!m_curentInteractible)
             return;
+
         //apply velocity
         Rigidbody targetBody = m_curentInteractible.GetComponent<Rigidbody>();
         targetBody.velocity = m_Pose.GetVelocity();
         targetBody.angularVelocity = m_Pose.GetAngularVelocity();
 
         //detatch
-        model.SetActive(true);
         m_joint.connectedBody = null;
 
         //clear
         m_curentInteractible.m_activeHand = null;
         m_curentInteractible = null;
+        controllerModel.gameObject.SetActive(true);
     }
     private Interactible GetNearestInteractible()
     {
         Interactible nearest = null;
         float minDistance = float.MaxValue;
         float distance = 0.0f;
-        foreach(Interactible interactible in m_contactInteracitbles)
+        foreach (Interactible interactible in m_contactInteracitbles)
         {
             distance = (interactible.transform.position - transform.position).sqrMagnitude;
 
