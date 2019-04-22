05bcbaf40b1decacaac62e8cde964cdec4517c33
Pieterww <pieterwinderlich@outlook.com>
2019-04-22 14:12:20 +0200

    Update SliderManager.cs

--- Assets/Resources/Scripts/UI/SliderManager.cs
+++ Assets/Resources/Scripts/UI/SliderManager.cs
@@ -8,14 +8,16 @@ public class SliderManager : MonoBehaviour
 {
 
     private AudioManager auManager;
-   [SerializeField]
+    public int listSlot = 0;
+    private List<string> _tagnames = new List<string>();
+    [SerializeField]
     private List<Slider> _sliderlist = new List<Slider>();
     // Start is called before the first frame update
     void Awake()
     {
 
         _sliderlist = gameObject.GetComponentsInChildren<Slider>().ToList();
-        auManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        auManager = GameObject.FindGameObjectWithTag(_tagnames[listSlot]).GetComponent<AudioManager>();
         for (int i = 0; i < _sliderlist.Count; i++)
         {
 
@@ -27,26 +29,31 @@ public class SliderManager : MonoBehaviour
 
         for (int i = 0; i < auManager.GetMList().Count; i++)
         {
+            for (int j = 0; j < auManager.GetMList().Count; j++)
+            {
+                _sliderlist[i].value = auManager.GetMList()[j].GetComponent<AudioSource>().volume;
+                Debug.Log(_sliderlist[i]);
+                Debug.Log(auManager.GetMList()[i]);
+            }
 
-            _sliderlist[0].value = auManager.GetMList()[0].GetComponent<AudioSource>().volume;
-            Debug.Log(_sliderlist[i]);
-            Debug.Log(auManager.GetMList()[i]);
+
 
         }
-        for (int i = 0; i < auManager.GetEList().Count; i++)
-        {
-            _sliderlist[1].value = auManager.GetEList()[0].GetComponent<AudioSource>().volume;
+        //    for (int i = 0; i < auManager.GetEList().Count; i++)
+        //    {
+        //        _sliderlist[1].value = auManager.GetEList()[0].GetComponent<AudioSource>().volume;
 
-            Debug.Log("Test1");
-        }
+
+        //    }
+        //}
+        //private void Start()
+        //{
+        //}
+
+        ////// Update is called once per frame
+        ////void Update()
+        ////{
+
+        ////}
     }
-    private void Start()
-    {
-    }
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
 }
